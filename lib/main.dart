fee03bf2bea59cf0682187cdacd6c777fe057adf
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-03-19 13:14:54 +0700

    Mengubah TaskPage menjadi home

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:secondproject/profile_page.dart';
+import 'package:secondproject/task_page.dart';
 
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const ProfilePage(),
+      home: const TaskPage(),
       debugShowCheckedModeBanner: false,
     );
   }
