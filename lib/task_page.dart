1c61f8ea6b2ca2dc18b519ef5863040048617e8a
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-03-20 15:11:14 +0700

    Menambahkan validasei pada input Task Name agar tidak kosong

--- lib/task_page.dart
+++ lib/task_page.dart
@@ -106,6 +106,12 @@ class _TaskPageState extends State<TaskPage> {
                         hintText: "Enter task name",
                         border: OutlineInputBorder(),
                       ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter some text";
+                        }
+                        return null;
+                      },
                     ),
                   ),
                 ],
